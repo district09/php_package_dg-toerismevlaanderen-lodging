24b13822976602072cf2cccfa6a1c12fd215226e
Peter Decuyper <peter.decuyper@digipolis.gent>
2019-08-22 13:59:34 +0200

    Added exception when response object does not contain the proper data.

--- src/Handler/CountHandler.php
+++ src/Handler/CountHandler.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace DigipolisGent\Toerismevlaanderen\Lodging\Handler;
 
+use DigipolisGent\API\Client\Exception\InvalidResponse;
 use DigipolisGent\API\Client\Handler\HandlerInterface;
 use DigipolisGent\API\Client\Response\ResponseInterface;
+use DigipolisGent\Toerismevlaanderen\Lodging\Exception\MissingResponseData;
 use DigipolisGent\Toerismevlaanderen\Lodging\Request\CountRequest;
 use DigipolisGent\Toerismevlaanderen\Lodging\Response\CountResponse;
 use Psr\Http\Message as Psr;
+use RuntimeException;
 
 /**
  * Handles the count request.
@@ -25,10 +28,30 @@ final class CountHandler implements HandlerInterface
 
     /**
      * @inheritDoc
+     *
+     * @throws \DigipolisGent\Toerismevlaanderen\Lodging\Exception\MissingResponseData
+     *   When the response dit not contain a count value.
      */
     public function toResponse(Psr\ResponseInterface $response): ResponseInterface
     {
         $data = json_decode($response->getBody()->getContents());
+        $this->assertDataIsComplete($data);
+
         return new CountResponse((int) $data->results->bindings[0]->count->value);
+    }
+
+    /**
+     * Assert that the response contains all necessary data.
+     *
+     * @param object
+     *
+     * @throws \DigipolisGent\Toerismevlaanderen\Lodging\Exception\MissingResponseData
+     *   When the response does not contain a count value.
+     */
+    private function assertDataIsComplete($data): void
+    {
+        if (!isset($data->results->bindings[0]->count->value)) {
+            throw MissingResponseData::count();
+        }
     }
 }
--- tests/Handler/CountHandlerTest.php
+++ tests/Handler/CountHandlerTest.php
@@ -2,12 +2,14 @@
 
 namespace DigipolisGent\Tests\Toerismevlaanderen\Lodging\Handler;
 
+use DigipolisGent\Toerismevlaanderen\Lodging\Exception\MissingResponseData;
 use DigipolisGent\Toerismevlaanderen\Lodging\Handler\CountHandler;
 use DigipolisGent\Toerismevlaanderen\Lodging\Request\CountRequest;
 use DigipolisGent\Toerismevlaanderen\Lodging\Response\CountResponse;
 use GuzzleHttp\Psr7\Stream;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
+use RuntimeException;
 
 /**
  * @covers \DigipolisGent\Toerismevlaanderen\Lodging\Handler\CountHandler
@@ -44,6 +46,25 @@ EOT;
         $expected = [CountRequest::class];
         $handler = new CountHandler();
         $this->assertEquals($expected, $handler->handles());
+    }
+
+    /**
+     * The handler throws exception if count variable is not within response.
+     *
+     * @test
+     */
+    public function exceptionIfResponseDoesNotContainCountValue(): void
+    {
+        $bodyMock = $this->prophesize(Stream::class);
+        $bodyMock->getContents()->willReturn('{}');
+        $responseMock = $this->prophesize(ResponseInterface::class);
+        $responseMock->getBody()->willReturn($bodyMock->reveal());
+
+        $handler = new CountHandler();
+
+        $this->expectException(MissingResponseData::class);
+        $this->expectExceptionMessage('Response data does not contain count value.');
+        $handler->toResponse($responseMock->reveal());
     }
 
     /**
