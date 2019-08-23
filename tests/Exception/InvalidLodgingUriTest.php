70f46119840ace11e8a6097f50be508402027531
Peter Decuyper <peter.decuyper@digipolis.gent>
2019-08-23 16:06:39 +0200

    Added missing tests.

---
+++ tests/Exception/InvalidLodgingUriTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DigipolisGent\Tests\Toerismevlaanderen\Lodging\Filter;
+
+use DigipolisGent\Toerismevlaanderen\Lodging\Exception\InvalidLodgingUri;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DigipolisGent\Toerismevlaanderen\Lodging\Exception\InvalidLodgingUri
+ */
+class InvalidLodgingUriTest extends TestCase
+{
+    /**
+     * From URI.
+     *
+     * @test
+     */
+    public function exceptionFromUriContainsProperMessage(): void
+    {
+        $uri = 'http://linked.toerismevlaanderen.be/fooBar';
+        $exception = InvalidLodgingUri::fromUri($uri);
+        $this->assertEquals(
+            '"http://linked.toerismevlaanderen.be/fooBar" is not a valid lodging URI.',
+            $exception->getMessage()
+        );
+        $this->assertSame(400, $exception->getCode());
+    }
+}
---
+++ tests/Exception/InvalidStarRatingTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DigipolisGent\Tests\Toerismevlaanderen\Lodging\Filter;
+
+use DigipolisGent\Toerismevlaanderen\Lodging\Exception\InvalidStarRating;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DigipolisGent\Toerismevlaanderen\Lodging\Exception\InvalidStarRating
+ */
+class InvalidStarRatingTest extends TestCase
+{
+    /**
+     * Proper message is set when created from invalid European format.
+     *
+     * @test
+     */
+    public function exceptionFromInvalidEuropeanFormatContainsProperMessage(): void
+    {
+        $exception = InvalidStarRating::notInEuropeanFormat('*****');
+        $this->assertEquals(
+            '"*****" is not in the European Hotelstar\'s Union format.',
+            $exception->getMessage()
+        );
+        $this->assertSame(400, $exception->getCode());
+    }
+}
